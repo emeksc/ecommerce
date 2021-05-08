0fe29ddec4e7bd1d2359674eb01adaa69b1046f3
emek <emek.cicek@kodnova.com>
2021-05-08 17:35:47 +0300

    create signin backend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,15 @@ import express from "express";
 import mongooese from "mongoose";
 import data from "./data.js";
 import userRouter from "./routers/userRouter.js";
-import productRouter from "./routers/productRouter.js"
+import productRouter from "./routers/productRouter.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongooese.connect("mongodb://localhost/ecommerce", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -17,9 +23,9 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.use((err,req,res,next)=>{
-  res.status(500).send({message:err.message})
-})
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
