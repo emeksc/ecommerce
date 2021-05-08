1246f129a5fe1bf83390366621d150bd24debca8
emek <emek.cicek@kodnova.com>
2021-05-08 11:46:45 +0300

    connect to mongodb and user collection add

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from "bcryptjs";
 const data = {
+  users: [
+    {
+      name: "Emek",
+      email: "admin@emek.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+    },
+    {
+      name: "Sedar",
+      email: "user@sedar.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       _id: "1",
@@ -6,7 +21,7 @@ const data = {
       category: "Category1",
       image: "/images/p1.png",
       price: 20,
-      countInStock:10,
+      countInStock: 10,
       brand: "brand",
       rating: 4.5,
       numReviews: 10,
@@ -18,7 +33,7 @@ const data = {
       category: "Category2",
       image: "/images/p2.png",
       price: 250,
-      countInStock:20,
+      countInStock: 20,
       brand: "brand",
       rating: 4.5,
       numReviews: 15,
@@ -30,7 +45,7 @@ const data = {
       category: "Category1",
       image: "/images/p3.png",
       price: 220,
-      countInStock:0,
+      countInStock: 0,
       brand: "brand",
       rating: 3,
       numReviews: 10,
@@ -42,7 +57,7 @@ const data = {
       category: "Category2",
       image: "/images/p4.png",
       price: 720,
-      countInStock:5,
+      countInStock: 5,
       brand: "brand",
       rating: 1.5,
       numReviews: 50,
@@ -54,7 +69,7 @@ const data = {
       category: "Category5",
       image: "/images/p5.png",
       price: 13,
-      countInStock:12,
+      countInStock: 12,
       brand: "brand",
       rating: 8,
       numReviews: 10,
@@ -66,7 +81,7 @@ const data = {
       category: "Category6",
       image: "/images/p6.png",
       price: 7,
-      countInStock:0,
+      countInStock: 0,
       brand: "brand",
       rating: 5,
       numReviews: 16,
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import mongooese from "mongoose";
 import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+mongooese.connect("mongodb://localhost/ecommerce", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
@@ -15,10 +22,14 @@ app.get("/api/products/:id", (req, res) => {
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
-
+app.use("/api/users", userRouter);
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
+
+app.use((err,req,res,next)=>{
+  res.status(500).send({message:err.message})
+})
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
